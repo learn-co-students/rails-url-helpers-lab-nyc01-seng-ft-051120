058192733c1112b1a524c52b1abfbcb41e8c2617
JulianCedric <julian.pormentilla@gmail.com>
2020-06-08 16:50:57 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,50 @@
+# class StudentsController < ApplicationController
+#   before_action :set_student, only: :show
+  
+#   def index
+#     @students = Student.all
+#   end
+
+#   def show
+#     @student = Student.find(params[:id])
+#   end
+
+#   def activate
+#     @student = Student.find(params[:id])
+#     # Locates a particular student
+#     @student.activate = !@student.active 
+#     @student.save 
+#     redirect_to student_path(@student)
+#     # Once the action is complete, the website redirects the user to another web page 
+#   end
+
+#   private
+
+#     def set_student
+#       @student = Student.find(params[:id])
+#     end
+# end
+
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active 
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+      def set_student
+        @student = Student.find(params[:id])
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#   resources :students, only: [:index, :show]
+#   # Points to the students model, and indicates to create routes for only the index and the show pages 
+
+#   get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
+
+# end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:index, :show]
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
